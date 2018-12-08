b2bc2b676f103c6244e29a478b05a36332c3ca04
Tommmy Collison <tommy@collison.ie>
2018-12-08 10:09:33 -0800

    updating projects.js

--- projects.js
+++ projects.js
@@ -1,26 +1,2 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/api/suppliers', (req, res) => {
-    res.json({
-        suppliers: [
-            'coffee roaster',
-            'food vendor',
-            'tea distributor'
-        ]
-    })
-});
-
-router.post('/api/suppliers', (req, res) => {
-    const supplier = req.body;
-    res.status(201).json({ created: supplier })
-});
-
-router.delete('/api/suppliers/:id', (req, res) => {
-    const id = req.params.id;
-    res.status(200).json({ deleted: id })
-});
-
-
-module.exports = router;
+// things go here
+// replacing sample code from Julie's notes
