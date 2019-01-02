38d7dba5f4d8b10dab3b037e93449a7d0ea2fbae
Tommmy Collison <tommy@collison.ie>
2019-01-02 15:44:35 -0600

    skeleton app

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-import { Server } from "tls";
-
 // Routing is all about handling requests for data, and the simplest is `get`:
 
 // Here's what that syntax looks like:
@@ -11,6 +9,16 @@ import { Server } from "tls";
 // When a get request is sent to '/' with an anonymous function, it returns a 200 status and some text.
 // Routing looks at the URL, returns stringified HTML.
 
+const express = require('express') // imports the express package we asked for when we did `yarn add express' 
 
+const server = express(); // creates the server
 
+server.get('/', (req, res) => {
+  res.send('Hello from inside the get');
+});
+
+// watch for connections on port 5000
+server.listen(5000, () =>
+  console.log('Server running on http://localhost:5000')
+);
 
