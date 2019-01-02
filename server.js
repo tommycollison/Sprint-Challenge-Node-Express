c6f092bda472a29cbb47f8e35379be2f1a08c824
Tommmy Collison <tommy@collison.ie>
2019-01-02 15:37:00 -0600

    setup server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+import { Server } from "tls";
+
+// Routing is all about handling requests for data, and the simplest is `get`:
+
+// Here's what that syntax looks like:
+
+// server.get('/', (req, res) => {
+//     res.status(200).send('<h1>Hello world</h1>');
+// });
+
+// When a get request is sent to '/' with an anonymous function, it returns a 200 status and some text.
+// Routing looks at the URL, returns stringified HTML.
+
+
+
+
